1cd2c4f488d7b0d9f6cb81a33a3e693f79e65fde
Akshay Vishwanaath Vallioor Ramachandran <akshay.ramachandran@panasonic.aero>
2018-11-29 13:10:39 -0800

    Update

--- index.py
+++ index.py
@@ -1,17 +1,21 @@
 import json
 import datetime
-
+import requests
+r = requests.post("https://l82nn2ozrk.execute-api.us-west-2.amazonaws.com/Prod/", data={'name': 'Akshay'})
+print(r)
 
 def handler(event, context):
     try:
+        print(event)
         print(event['requestContext']['identity']['userArn'])
         caller_name=event['requestContext']['identity']['userArn']
         split_name=caller_name.split("/")
         greet_name=split_name[-1]
     except:
+        print("ARN not found")
         print(event)
         greet_name='User'
-    greet= 'Hello, '+ greet_name
+    
     data = {
         'output': 'Hello World',
         'name': greet_name,
