cc5807e794f20e31d117b54813dea910d39da2cc
Akshay Vishwanaath Vallioor Ramachandran <akshay.ramachandran@panasonic.aero>
2018-11-29 10:19:00 -0800

    Updated the Name field

--- index.py
+++ index.py
@@ -3,9 +3,11 @@ import datetime
 
 
 def handler(event, context):
+    name=input("Enter your name: ")
     data = {
         'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'name': f'Hello, {name}'
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
